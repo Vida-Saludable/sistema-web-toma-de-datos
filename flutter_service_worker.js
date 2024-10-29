c34241fb147057d3fa61f6aa427d76705c3c6018
DESKTOP-VTA8OCP\JUAN JOSE <juan.merlos@uab.edu.bo>
2024-10-29 11:50:58 -0400

    cambios en el sistema

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0e8233a28c676c412a8c53b15e4f36d9
 "assets/assets/images/error.png": "34a3c86257800bf5e90595c112370b19",
 "assets/assets/images/profile.jpg": "f9bead6a645c076fb2d75a6b9dd6689b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "3163c2cf22f8cde0926379e36814acc4",
+"assets/fonts/MaterialIcons-Regular.otf": "af22c1afdd3a6b07856134d76dc39843",
 "assets/NOTICES": "3db8da5be87b0e58aeacdfc3c0368394",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0e8233a28c676c412a8c53b15e4f36d9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f912c95339fc98ed5e21821ff7162e28",
-"/": "f912c95339fc98ed5e21821ff7162e28",
-"main.dart.js": "b54fbb08e50b67b0ee948115140c50e7",
+"index.html": "d56cac65941f4a6037a6b69412454129",
+"/": "d56cac65941f4a6037a6b69412454129",
+"main.dart.js": "53acf7743c5ce80b17031b842711a240",
 "manifest.json": "ab3054fdb67865f61e6ef7c28ff07af2",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"};
 // The application shell files that are downloaded before a service worker can
